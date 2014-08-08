cba5bce8fe16dc35046e7933c956f1abce7a0b1b
Dhvanan Shah <dhvanan@gmail.com>
2014-08-09 01:53:24 +0530

    monochromatic updated

--- monochromatic.js
+++ monochromatic.js
@@ -3,19 +3,26 @@ Pallette.prototye.monochromatic = function (rgb,num_col) {
     var green = rgb.g;
     var blue = rgb.b;
     
-    var step = 255/num_col;
+    var step = 255/(num_col+1);
     
     var color_arr = [{rgb.r,rgb.g,rgb.b}];
     var i;
-    for(i=0;i<num_col;i++)
+    for(i=1 ; i < num_col/2 ; i++)
     {
-        if(red>255 || green>255 || blue>255) 
-        {
-            step=-step;
-            red = rgb.r;
-            green = rgb.g;
-            blue = rgb.b;   
-        }
+        if(red<255 || green<255 || blue<255) 
+            break;
+        
+        red=red+step;
+        green=green+step;
+        blue=blue+step;
+        
+        color_arr.push({red,green,blue});
+    }
+    
+    for( i=1 ; i < (num_col-num_col/2) ; i++)
+    {
+        if(red>0 || green>0 || blue>0) 
+            break;
         
         red=red+step;
         green=green+step;
