af583db78ca499b00196ffc9b21393bcde36045c
Dhvanan Shah <dhvanan@gmail.com>
2014-08-08 23:53:06 +0530

    Added complementary color chooser

--- complementary.js
+++ complementary.js
@@ -1,4 +1,11 @@
-Pallette.prototye.complementary = function (colorHash) {
-	var resultArray = [];
-	return resultArray;
+Pallette.prototye.complementary = function (rgb) {
+	var red = rgb.r;
+    var green = rgb.g;
+    var blue = rgb.b;
+    
+    return [{
+        r: 255 - red;
+        g: 255 - green;
+        b: 255 - blue;
+    }];
 };
