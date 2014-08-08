e3acddcbff6c17ca01c5b0a070abbc181e9d54ac
Amal Antony <amalantony06@gmail.com>
2014-08-08 22:53:49 +0530

    Added contantMix color pallette algo

--- randomize.js
+++ randomize.js
@@ -5,22 +5,49 @@ function getRandom() {
 
 function Pallette() {}
 
-Pallette.prototype.randomize = function(rgbVal) {
+Pallette.prototype.randomizeMix = function (rgbVal) {
 	/*
 		Input sample: {r: 234, b: 216, g: 110}
 	*/
 	var red = getRandom();
 	var green = getRandom();
 	var blue = getRandom();
-	
+
 	// mix colors 
 	if (rgbVal !== null) {
-		red = (red + rgbVal.r) / 2;
-		green = (green + rgbVal.g) / 2;
-		blue = (blue + rgbVal.b) / 2;
+		red = parseInt((red + rgbVal.r) / 2);
+		green = parseInt((green + rgbVal.g) / 2);
+		blue = parseInt((blue + rgbVal.b) / 2);
 	}
-	
+
 	return {
-		r: red, g: green, b: blue
+		r: red,
+		g: green,
+		b: blue
+	};
+};
+
+Pallette.prototype.constantMix = function (rgbVal, cmix) {
+	/*
+		Input sample: {r: 234, b: 216, g: 110}
+	*/
+	var red, green, blue;
+	if (cmix) {
+		red = cmix.r || 216;
+		green = cmix.g || 115;
+		blue = cmix.b || 95;
+	}
+
+	// mix colors 
+	if (rgbVal !== null) {
+		red = parseInt((red + rgbVal.r) / 2);
+		green = parseInt((green + rgbVal.g) / 2);
+		blue = parseInt((blue + rgbVal.b) / 2);
+	}
+
+	return {
+		r: red,
+		g: green,
+		b: blue
 	};
 };
